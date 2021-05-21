adaa791d64db8007b5f6d7546fcf30033adad04e
Emir Gocturk <emir.gocturk@axign.nl>
2021-05-21 16:55:13 +0200

    Get process id

--- esp/esp.cpp
+++ esp/esp.cpp
@@ -15,7 +15,26 @@ int screenY = GetSystemMetrics(SM_CYSCREEN);
 
 DWORD GetProcId(const wchar_t* procName)
 {
-    
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+
+        if (Process32First(hSnap, &procEntry))
+        {
+            do
+            {
+                if (!_wcsicmp(procEntry.szExeFile, procName))
+                {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry);
+        }
+    }
 }
 
 int main()
